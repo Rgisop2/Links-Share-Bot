a445d68d8c18d2d1d04f514d390d0f24e74e688c
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2025-12-05 23:41:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,32 +56,33 @@ START_IMG = "https://telegra.ph/file/f3d3aff9ec422158feb05-d2180e3665e0ac4d32.jp
 # ---------- MESSAGES ----------
 START_MSG = os.environ.get(
     "START_MESSAGE",
-    "<b>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴀᴅᴠᴀɴᴄᴇᴅ ʟɪɴᴋs sʜᴀʀɪɴɢ ʙᴏᴛ."
-    "<blockquote>Maintained by: <a href='https://t.me/codeflix_bots'>ʏᴀᴛᴏ</a></blockquote></b>"
+    "𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝘼𝘿𝙑𝘼𝙉𝘾𝙀𝘿 𝙇𝙄𝙉𝙆𝙎 𝙎𝙃𝘼𝙍𝙄𝙉𝙂 𝘽𝙊𝙏."
 )
 
 HELP = os.environ.get(
     "HELP_MESSAGE",
-    "<b><blockquote expandable>» Creator: <a href=https://t.me/proyato>Yato</a>\n"
-    "» Our Community: <a href=https://t.me/otakuflix_network>Flix Network</a>\n"
-    "» Anime Channel: <a href=https://t.me/animes_cruise>Anime Cruise</a>\n"
-    "» Ongoing Anime: <a href=https://t.me/Ongoing_cruise>Ongoing cruise</a>\n"
-    "» Developer: <a href=https://t.me/onlyyuji>Yuji</a></b>"
+    "<b><blockquote expandable>"
+    "» ᴀɴɪᴍᴇ ɢʀᴏᴜᴘ: <a href='https://t.me/RG_Anime_Group_Chat'>ᴀɴɪᴍᴇ ɢʀᴏᴜᴘ</a>\n"
+    "» ᴀɴɪᴍᴇ ʜɪɴᴅɪ: <a href='https://t.me/Anime_Link_robot?start=req_LTEwMDIxMjI0NDQ0MTU'>ᴀɴɪᴍᴇ ʜɪɴᴅɪ</a>"
+    "</blockquote></b>"
 )
 
 ABOUT = os.environ.get(
     "ABOUT_MESSAGE",
-    "<b><blockquote expandable>This bot is developed by Yato (@ProYato) to securely share Telegram links.</b>"
+    "<b><blockquote expandable>"
+    "ᴛʜɪs ʙᴏᴛ ʜᴇʟᴘs ʏᴏᴜ sᴇᴄᴜʀᴇʟʏ sʜᴀʀᴇ ᴛᴇʟᴇɢʀᴀᴍ ʟɪɴᴋs."
+    "</blockquote></b>"
 )
 
-ABOUT_TXT = """<b>›› ᴄᴏᴍᴍᴜɴɪᴛʏ: <a href='https://t.me/otakuflix_network'>ᴏᴛᴀᴋᴜғʟɪx</a>
-<blockquote expandable>›› ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/codeflix_bots'>Click Here</a>
-›› ᴏᴡɴᴇʀ: <a href='https://t.me/cosmic_freak'>Yato</a>
+ABOUT_TXT = """
+<b>›› ᴄᴏᴍᴍᴜɴɪᴛʏ: <a href='https://t.me/RG_Anime_Group_Chat'>ᴀɴɪᴍᴇ ɢʀᴏᴜᴘ</a>
+<blockquote expandable>
+›› ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/Anime_Link_robot?start=req_LTEwMDIxMjI0NDQ0MTU'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>
 ›› ʟᴀɴɢᴜᴀɢᴇ: Python 3
 ›› ʟɪʙʀᴀʀʏ: Pyrogram v2
 ›› ᴅᴀᴛᴀʙᴀsᴇ: MongoDB
-›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @ProYato</b></blockquote>"""
-
+</b></blockquote>
+"""
 CHANNELS_TXT = """<b>›› Anime: <a href='https://t.me/Anime_Link_robot?start=req_LTEwMDIxMjI0NDQ0MTU'>Anime in Hindi</a>
 <blockquote expandable>›› Movies: <a href='https://t.me/Anime_Link_robot?start=req_LTEwMDE3Mjg1Mzk2ODg'>Anime Movie Hindi</a>
 ›› Adult: <a href='https://t.me/Hanime_tv'>CornHub</a>
